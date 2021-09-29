ba74094e59cf7679a8d31532fe9fcdee3beceeee
natescool05 <70679243+natescool05@users.noreply.github.com>
2021-09-29 10:04:40 -0600

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example2" {
   ami           = "ami-0c2b8ca1dad447f8a"
   instance_type = "t2.large"
 }
